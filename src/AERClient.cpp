e7d3ac9c567e5f897c21485f4ba9a6d5f90e9f3b
Benjamin <sabean.ben@gmail.com>
2018-05-07 11:17:59 -0300

    Allows user to break out of Wi-Fi reconnect loop.

--- src/AERClient.cpp
+++ src/AERClient.cpp
@@ -1,10 +1,10 @@
 
-#include <AERClient.h>            // Library for One-Wire interface 
+#include <AERClient.h>            // Library for One-Wire interface
 
 // Constructor that that stores Module ID
 AERClient::AERClient(int ID)
 {
-  _ID = ID;
+    _ID = ID;
 }
 
 // Constructor that that stores Module ID and timeout value
@@ -15,37 +15,37 @@ AERClient::AERClient(int ID, unsigned int timeout)
 }
 
 /*
-  Establishes connection to WiFi access point with parameters provided
-  Connects to MQTT broker - aerlab.ddns.net
-*/
+ Establishes connection to WiFi access point with parameters provided
+ Connects to MQTT broker - aerlab.ddns.net
+ */
 bool AERClient::init(const char* ssid, const char* password)
 {
-  uint8_t mac[6], status;
-  // Saving Wifi Parametrs
-  _ssid = ssid;
-  _password = password;
-  // Creating PubSubClient Object
-  _client = new PubSubClient(_server, _port, _espClient);
-  _client->setServer(_server, 1883);
-  // Generating ID for MQTT broker
-  WiFi.macAddress(mac);
-  clientName = clientName + "esp8266-" + macToStr(mac) + "-" + String(_ID);
-  // Establish WiFi Connection
-  WiFi.disconnect();
-  WiFi.mode(WIFI_STA);
-  if(!wifiConnect()) {
-    return false;
-  }
-  //check for MQTT Connection
-  if (!_client->connected()) reconnect();
-  _client->loop();
-  return true;
+    uint8_t mac[6], status;
+    // Saving Wifi Parametrs
+    _ssid = ssid;
+    _password = password;
+    // Creating PubSubClient Object
+    _client = new PubSubClient(_server, _port, _espClient);
+    _client->setServer(_server, 1883);
+    // Generating ID for MQTT broker
+    WiFi.macAddress(mac);
+    clientName = clientName + "esp8266-" + macToStr(mac) + "-" + String(_ID);
+    // Establish WiFi Connection
+    WiFi.disconnect();
+    WiFi.mode(WIFI_STA);
+    if(!wifiConnect()) {
+        return false;
+    }
+    //check for MQTT Connection
+    if (!_client->connected()) reconnect();
+    _client->loop();
+    return true;
 }
 
 // Destructor to de-allocate memory
 AERClient::~AERClient()
 {
-  delete _client;
+    delete _client;
 }
 
 void AERClient::disableReconnect() {
@@ -57,95 +57,94 @@ void AERClient::enableReconnect() {
 }
 
 /*
-  Published Data to AERLab server
-  First parameter - Topic, second - Payload
-*/
+ Published Data to AERLab server
+ First parameter - Topic, second - Payload
+ */
 bool AERClient::publish(String topic, String payload)
 {
-  bool _result = false;
-  if (WiFi.localIP().toString() != "0.0.0.0")
-  {
-    reconnect();
-    _client->loop();
-    _result = _client->publish((String(_ID) + "/" + topic).c_str(), payload.c_str(), 2);
-  }
-  else
-  {
-    if(!wifiConnect()) {
-      return false;
+    bool _result = false;
+    if (WiFi.localIP().toString() != "0.0.0.0")
+    {
+        reconnect();
+        _client->loop();
+        _result = _client->publish((String(_ID) + "/" + topic).c_str(), payload.c_str(), 2);
     }
-    reconnect();                    //check for MQTT Connection
-    _client->loop();
-  }
-  return _result;
+    else
+    {
+        if(!wifiConnect()) {
+            return false;
+        }
+        reconnect();                    //check for MQTT Connection
+        _client->loop();
+    }
+    return _result;
 }
 
 
 /*
-  Connect to Wi-Fi access point
-*/
+ Connect to Wi-Fi access point
+ */
 bool AERClient::wifiConnect()
 {
     // Attempt to connect to access point
     WiFi.begin(_ssid, _password);
     uint8_t i = 0;
     // Wait for Wi-Fi to connect or for timeout
-    while (WiFi.localIP().toString() == "0.0.0.0" && i <= _timeout)
+    while (WiFi.localIP().toString() == "0.0.0.0" && i <= _timeout && _reconnectFlag)
     {
         delay(1000);
         Serial.print(".");
         i++;
     }
-    if(i > _timeout) {
+    if(i > _timeout || !_reconnectFlag)
+    {
         return false;  // Could not connect
     }
     return true;
 }
 
 /*
-  Reconnecting to MQTT broker
-*/
+ Reconnecting to MQTT broker
+ */
 void AERClient::reconnect()
 {
-  if(_reconnectFlag) {
-    // Loop until we're reconnected or time out
+    // Loop until we're reconnected
     while (!_client->connected())
     {
         if (!_client->connect(clientName.c_str(), mqtt_user, mqtt_pswd))
             delay(10 * 1000);
     }
-  }
 }
 
 /*
-  MAC address used as MQTT identification
-*/
+ MAC address used as MQTT identification
+ */
 String AERClient::macToStr(const uint8_t* mac)
 {
-  String result;
-  for (int i = 0; i < 6; ++i) {
-    result += String(mac[i], 16);
-    if (i < 5)
-      result += ':';
-  }
-  return result;
+    String result;
+    for (int i = 0; i < 6; ++i) {
+        result += String(mac[i], 16);
+        if (i < 5)
+            result += ':';
+    }
+    return result;
 }
 
 /*
-  Used to Debug library operation
-  Requeires Serial.begin()
-*/
+ Used to Debug library operation
+ Requeires Serial.begin()
+ */
 void AERClient::debug()
 {
-  Serial.println("\n------- AERClient DEBUG ------- ");
-  Serial.print("WiFi IP address: ");
-  Serial.println(WiFi.localIP());
-  Serial.println("Module Name: " + clientName);
-  Serial.println("Connecting to: " + String(_ssid));
-  Serial.println("With password: " + String(_password));
-  Serial.println("MQTT Status: " + String(_client->state()));
-  Serial.println("------- WiFi DEBUG ------- ");
-  WiFi.printDiag(Serial);
+    Serial.println("\n------- AERClient DEBUG ------- ");
+    Serial.print("WiFi IP address: ");
+    Serial.println(WiFi.localIP());
+    Serial.println("Module Name: " + clientName);
+    Serial.println("Connecting to: " + String(_ssid));
+    Serial.println("With password: " + String(_password));
+    Serial.println("MQTT Status: " + String(_client->state()));
+    Serial.println("------- WiFi DEBUG ------- ");
+    WiFi.printDiag(Serial);
 }
 
 
